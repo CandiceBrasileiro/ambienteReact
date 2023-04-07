e7f01a61809276319b4e64950096bba70fd4ae90
CandiceBrasileiro <candicelopes88@gmail.com>
2023-04-07 10:09:49 -0300

    forms desestruturados

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-
-const Titulo = (props) => {
-  // const Titulo = ({cor, texto}) => { assim seria desestruturado
-  //return <h1 style={{color: cor}}>{texto}</h1>
-  return <h1 style={{color: props.cor}}>{props.texto}</h1>
-}
+import Form from './Form/Form';
 
 const App = () => {
   return <div>
-    <Titulo cor="red" texto='Meu título 1'/>
-    <Titulo cor="blue" texto='Meu título 1'/>
-    <Titulo texto='Meu título 1'/>
+    <Form />
   </div>;
 }
 
--- src/form/Form.js
+++ src/form/Form.js
@@ -5,15 +5,9 @@ import Input from './Input'
 const Form = () => {
   return (
     <form >
-      <p>
-        <label htmlFor="nome">Nome</label>
-        <Input />
-      </p>
-      <p>
-       <label htmlFor="email">E-mail</label>
-       <Input />
-      </p>
-       <Button />
+      <Input id='email' label="email" required/>
+      <Input id='senha' type="password" label="senha"/>
+      <Button />
     </form>
   )
 }
