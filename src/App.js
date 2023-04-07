213342d7627dc003f2359b347a30fe58941ff72c
CandiceBrasileiro <candicelopes88@gmail.com>
2023-04-07 11:23:45 -0300

    desafio componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import Form from './Form/Form';
+import Header from './DesafioC/Header';
+import Home from './DesafioC/Home';
+import Produtos from './DesafioC/Produtos';
+
+// Replique a interface como a apresentada na aula
+// Utilize a array abaixo para mostrar os produtos
+// Quebre em componentes o que precisar ser reutilizado
+// Dica: const { pathname } = window.location; (puxa o caminho do URL)
+
+
 
 const App = () => {
-  return <div>
-    <Form />
-  </div>;
-}
+  
+  const {pathname} = window.location;
+
+  let Pagina;
+  if(pathname === '/produtos'){
+    Pagina = Produtos
+  } else {
+    Pagina = Home
+  }
+  return (
+  <section>
+    <Header />
+    <Pagina />
+  </section>
+  );
+};
 
 export default App;
 
